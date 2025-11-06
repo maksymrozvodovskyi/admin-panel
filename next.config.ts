9c2c837d21dedb9e48cda9518d6370bb80b44e8a
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-11-06 14:12:41 +0200

    add PWA to application

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWAModule = withPWA({
+	dest: 'public',
+})
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+}
+
+export default withPWAModule(nextConfig)
