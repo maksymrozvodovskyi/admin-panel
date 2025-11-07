035278e8a6fc8c9736167cd47ebcdc9c2e000a42
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-11-07 13:03:29 +0200

    feat: implement login with cookies and middleware protection

--- app/(auth routes)/sign-in/page.tsx
+++ app/(auth routes)/sign-in/page.tsx
@@ -1,33 +1,23 @@
 'use client'
-
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { loginRequest } from '@/lib/api/login'
-import { useAuthStore } from '@/lib/store/authStore'
 
 export default function SignInPage() {
 	const [email, setEmail] = useState('')
-
 	const [password, setPassword] = useState('')
-
 	const [error, setError] = useState('')
-
 	const router = useRouter()
-
-	const { loginWithServer } = useAuthStore()
 
 	const handleLogin = async (e: React.FormEvent) => {
 		e.preventDefault()
 		setError('')
-
 		const data = await loginRequest(email, password)
 
 		if (!data.success) {
 			setError(data.message || 'Invalid credentials')
 			return
 		}
-
-		loginWithServer(data.admin)
 
 		router.push('/dashboard')
 	}
@@ -36,7 +26,6 @@ export default function SignInPage() {
 		<div className='flex items-center justify-center h-screen bg-gray-100'>
 			<form onSubmit={handleLogin} className='bg-white p-8 rounded-lg shadow-md w-80 flex flex-col gap-4'>
 				<h1 className='text-2xl font-semibold text-center'>Admin Login</h1>
-
 				<input
 					type='email'
 					value={email}
@@ -51,9 +40,7 @@ export default function SignInPage() {
 					placeholder='Password'
 					className='border rounded px-3 py-2'
 				/>
-
 				{error && <p className='text-red-600 text-sm text-center'>{error}</p>}
-
 				<button
 					type='submit'
 					className='bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition cursor-pointer'
